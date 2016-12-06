419e1e49575429b1b928cbccffbf0e1e4f508c3d
Alex <whitedawn1@hotmail.com>
2016-12-06 09:42:52 -0600

    terminology corrected

--- src/main/java/SVCollectionGUI.java
+++ src/main/java/SVCollectionGUI.java
@@ -28,16 +28,16 @@ public class SVCollectionGUI extends JFrame {
     final double GOLDS_IN_DARKNESS = 23;
     final double SILVERS_IN_DARKNESS = 31;
     final double BRONZES_IN_DARKNESS = 46;
-    //dust create/liquefy values
-    final double LEGENDARY_DUST_LIQUEFY = 1000;
-    final double GOLD_DUST_LIQUEFY = 250;
-    final double SILVER_DUST_LIQUEFY = 50;
-    final double BRONZE_DUST_LIQUEFY = 10;
+    //card create/liquefy vial values
+    final double LEGENDARY_VIAL_LIQUEFY = 1000;
+    final double GOLD_VIAL_LIQUEFY = 250;
+    final double SILVER_VIAL_LIQUEFY = 50;
+    final double BRONZE_VIAL_LIQUEFY = 10;
 
-    final double LEGENDARY_DUST_CREATE = 3500;
-    final double GOLD_DUST_CREATE = 800;
-    final double SILVER_DUST_CREATE = 200;
-    final double BRONZE_DUST_CREATE = 50;
+    final double LEGENDARY_VIAL_CREATE = 3500;
+    final double GOLD_VIAL_CREATE = 800;
+    final double SILVER_VIAL_CREATE = 200;
+    final double BRONZE_VIAL_CREATE = 50;
 
     SVCollectionGUI(final SVCollectionDM svCollectionDM) {
         super("Shadowverse Collection");
@@ -62,33 +62,33 @@ public class SVCollectionGUI extends JFrame {
                 double avgDustGainDarkness = 0.0;
 
                 int duplicatesStandard = SVCollectionDB.getNumberOfDuplicates("Standard", "Legendary");
-                avgDustGainStandard += (duplicatesStandard/LEGENDARIES_IN_STANDARD * LEGENDARY_DUST_LIQUEFY + ((LEGENDARIES_IN_STANDARD - duplicatesStandard)/LEGENDARIES_IN_STANDARD) * LEGENDARY_DUST_CREATE) * CHANCE_OF_LEGENDARY * 8;
+                avgDustGainStandard += (duplicatesStandard/LEGENDARIES_IN_STANDARD * LEGENDARY_VIAL_LIQUEFY + ((LEGENDARIES_IN_STANDARD - duplicatesStandard)/LEGENDARIES_IN_STANDARD) * LEGENDARY_VIAL_CREATE) * CHANCE_OF_LEGENDARY * 8;
 
                 duplicatesStandard = SVCollectionDB.getNumberOfDuplicates("Standard", "Gold");
-                avgDustGainStandard += (duplicatesStandard/GOLDS_IN_STANDARD * GOLD_DUST_LIQUEFY + ((GOLDS_IN_STANDARD - duplicatesStandard)/GOLDS_IN_STANDARD) * GOLD_DUST_CREATE) * CHANCE_OF_GOLD * 8;
+                avgDustGainStandard += (duplicatesStandard/GOLDS_IN_STANDARD * GOLD_VIAL_LIQUEFY + ((GOLDS_IN_STANDARD - duplicatesStandard)/GOLDS_IN_STANDARD) * GOLD_VIAL_CREATE) * CHANCE_OF_GOLD * 8;
 
                 duplicatesStandard = SVCollectionDB.getNumberOfDuplicates("Standard", "Silver");
-                avgDustGainStandard += (duplicatesStandard/SILVERS_IN_STANDARD * SILVER_DUST_LIQUEFY + ((SILVERS_IN_STANDARD - duplicatesStandard)/SILVERS_IN_STANDARD) * SILVER_DUST_CREATE) * (CHANCE_OF_SILVER_NOT_8TH * 7 + CHANCE_OF_SILVER_8TH);
+                avgDustGainStandard += (duplicatesStandard/SILVERS_IN_STANDARD * SILVER_VIAL_LIQUEFY + ((SILVERS_IN_STANDARD - duplicatesStandard)/SILVERS_IN_STANDARD) * SILVER_VIAL_CREATE) * (CHANCE_OF_SILVER_NOT_8TH * 7 + CHANCE_OF_SILVER_8TH);
 
                 duplicatesStandard = SVCollectionDB.getNumberOfDuplicates("Standard", "Bronze");
-                avgDustGainStandard += (duplicatesStandard/BRONZES_IN_STANDARD * BRONZE_DUST_LIQUEFY + ((BRONZES_IN_STANDARD - duplicatesStandard)/BRONZES_IN_STANDARD) * BRONZE_DUST_CREATE) * CHANCE_OF_BRONZE * 7;
+                avgDustGainStandard += (duplicatesStandard/BRONZES_IN_STANDARD * BRONZE_VIAL_LIQUEFY + ((BRONZES_IN_STANDARD - duplicatesStandard)/BRONZES_IN_STANDARD) * BRONZE_VIAL_CREATE) * CHANCE_OF_BRONZE * 7;
 
 
                 int duplicatesDarkness = SVCollectionDB.getNumberOfDuplicates("Darkness", "Legendary");
-                avgDustGainDarkness += (duplicatesDarkness/LEGENDARIES_IN_DARKNESS * LEGENDARY_DUST_LIQUEFY + ((LEGENDARIES_IN_DARKNESS - duplicatesDarkness)/LEGENDARIES_IN_DARKNESS) * LEGENDARY_DUST_CREATE) * CHANCE_OF_LEGENDARY * 8;
+                avgDustGainDarkness += (duplicatesDarkness/LEGENDARIES_IN_DARKNESS * LEGENDARY_VIAL_LIQUEFY + ((LEGENDARIES_IN_DARKNESS - duplicatesDarkness)/LEGENDARIES_IN_DARKNESS) * LEGENDARY_VIAL_CREATE) * CHANCE_OF_LEGENDARY * 8;
 
                 duplicatesDarkness = SVCollectionDB.getNumberOfDuplicates("Darkness", "Gold");
-                avgDustGainDarkness += (duplicatesDarkness/GOLDS_IN_DARKNESS * GOLD_DUST_LIQUEFY + ((GOLDS_IN_DARKNESS - duplicatesDarkness)/GOLDS_IN_DARKNESS) * GOLD_DUST_CREATE) * CHANCE_OF_GOLD * 8;
+                avgDustGainDarkness += (duplicatesDarkness/GOLDS_IN_DARKNESS * GOLD_VIAL_LIQUEFY + ((GOLDS_IN_DARKNESS - duplicatesDarkness)/GOLDS_IN_DARKNESS) * GOLD_VIAL_CREATE) * CHANCE_OF_GOLD * 8;
 
                 duplicatesDarkness = SVCollectionDB.getNumberOfDuplicates("Darkness", "Silver");
-                avgDustGainDarkness += (duplicatesDarkness/SILVERS_IN_DARKNESS * SILVER_DUST_LIQUEFY + ((SILVERS_IN_DARKNESS - duplicatesDarkness)/SILVERS_IN_DARKNESS) * SILVER_DUST_CREATE) * (CHANCE_OF_SILVER_NOT_8TH * 7 + CHANCE_OF_SILVER_8TH);
+                avgDustGainDarkness += (duplicatesDarkness/SILVERS_IN_DARKNESS * SILVER_VIAL_LIQUEFY + ((SILVERS_IN_DARKNESS - duplicatesDarkness)/SILVERS_IN_DARKNESS) * SILVER_VIAL_CREATE) * (CHANCE_OF_SILVER_NOT_8TH * 7 + CHANCE_OF_SILVER_8TH);
 
                 duplicatesDarkness = SVCollectionDB.getNumberOfDuplicates("Darkness", "Bronze");
-                avgDustGainDarkness += (duplicatesDarkness/BRONZES_IN_DARKNESS * BRONZE_DUST_LIQUEFY + ((BRONZES_IN_DARKNESS - duplicatesDarkness)/BRONZES_IN_DARKNESS) * BRONZE_DUST_CREATE) * CHANCE_OF_BRONZE * 7;
+                avgDustGainDarkness += (duplicatesDarkness/BRONZES_IN_DARKNESS * BRONZE_VIAL_LIQUEFY + ((BRONZES_IN_DARKNESS - duplicatesDarkness)/BRONZES_IN_DARKNESS) * BRONZE_VIAL_CREATE) * CHANCE_OF_BRONZE * 7;
 
 
                 JOptionPane.showMessageDialog(rootPanel, "Standard pack value: " + avgDustGainStandard +
-                        " dust\nDarkness pack value: " + avgDustGainDarkness + " dust");
+                        " vials\nDarkness pack value: " + avgDustGainDarkness + " vials");
             }
         });
 
